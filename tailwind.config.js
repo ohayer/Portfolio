b0ec1bf2773fbcc72ce3c5b490783219f8a66a79
ohayer <olik03.2003@wp.pl>
2025-01-04 19:53:31 +0100

    data structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+        chakra: ['"Chakra Petch"', 'sans-serif'],
+      },
+      maxHeight: {
+        terms: '250vh',
+      },
+      colors: {
+        customGray: '#202020',
+      },
+      scale: {
+        '60': '0.6',
+        '95': '.95',
+        '85': '.85',
+      },
+      colors: {
+        'lagune': '#007ACC',
+        'customBlack': '#0d0d0d',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
